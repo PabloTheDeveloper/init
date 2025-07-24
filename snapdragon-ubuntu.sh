8db4b8c9f2ffe8a1e8f7c44cc84c9225ae0c88ab
pablothedeveloper <30012721+pablothedeveloper@users.noreply.github.com>
2025-07-24 18:51:15 -0400

    Update to get i3blocks to work

---
+++ snapdragon-ubuntu.sh
@@ -0,0 +1,18 @@
+sudo apt-get install fonts-font-awesome fish less
+sudo apt-get copyq
+# Startup copyq on each laptop startup.
+# install i3blocks pre-req
+sudo apt-get install autoconf
+# install i3blocks from
+# source (apt is broken)
+cd
+git clone https://github.com/vivien/i3blocks
+cd i3blocks
+./autogen.sh
+./configure
+make
+make install
+# install disk_usage pre-req (o.g
+# python)
+sudo apt-get install python-is-python3
+
