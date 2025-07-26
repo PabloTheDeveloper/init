74ab1244f84253a0ddbb7c122917b409ea1770a9
pablothedeveloper <30012721+pablothedeveloper@users.noreply.github.com>
2025-07-26 14:58:41 -0400

    Add font/icons

--- snapdragon-ubuntu.sh
+++ snapdragon-ubuntu.sh
@@ -18,3 +18,14 @@ sudo apt-get install python-is-python3
 
 # A better cat
 sudo apt-get install batcat
+
+# To install fonts & icons...
+# Taken from: https://gist.github.com/matthewjberger/7dd7e079f282f8138a9dc3b045ebefa0
+# #/bin/bash
+# install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
+echo "[-] Download fonts [-]"
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
+unzip DroidSansMono.zip -d ~/.fonts
+fc-cache -fv
+echo "done!"
